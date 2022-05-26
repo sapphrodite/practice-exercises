2f40c15c9e1170a62ca2dbe0dc960a40b6bdc7dd
ctaylor127 <ctaylor9@umassd.edu>
2022-05-26 15:20:12 -0400

    Initial import

---
+++ Fibonacci.py
@@ -0,0 +1,27 @@
+# Fibonacci implementation
+
+import math
+list1 = []
+
+""" User input """
+N = int(input("Enter a value for N: "))
+
+for x in range(0, N+1):
+    x = list1.append(0)
+    y = list1.append(1)
+    s = math.fsum(list1,1)
+    z = list1.append(s)
+    ss = math.fsum(list1+z)
+    zz = list1.append(ss)
+
+print(list1)
+
+
+"""
+ x = list1.append(0)
+    y = list1.append(1)
+    s = math.fsum(list1,1)
+    z = list1.append(s)
+    ss = math.fsum(list1+z)
+    zz = list1.append(ss)
+"""
