2cebba79c86600416789d4ec601ec5136dc767b4
ctaylor127 <ctaylor9@umassd.edu>
2022-06-03 16:21:45 -0400

    re Upload 1

--- Project4.py
+++ Project4.py
@@ -1,34 +1,19 @@
 # Project 4 by Christian Taylor
 import math
 
-list1 = []
-list2 = []
-m=1
-n=3
+d = 1
+count = 0
+n = 0
 
-""" User input """
 iter = int(input("Enter the number of iterations: "))
 
-""" Positive List """
-for x in range(1, iter+1, 2):
-    pattern1 = (1/m)
-    list1.append(pattern1)
-    m += 4
+for x in range(1, iter + 1):
+    frac = (1 / d)
+    n += 1
+    if (n % 2) == 0:
+        frac *= -1
+    count += frac
+    d += 2
 
-""" Negitive List """
-for y in range(1, iter, 2):
-    pattern2 = (1/n) * (-1)
-    list2.append(pattern2) 
-    n += 4
-
-"""Combine both lists into one list"""
-comb = list1.extend(list2)
-
-"""Sum all values in list"""
-sum = sum(list1)
-
-""" Multiply result by 4 """ 
-pi = sum*4
-
-""" Print result """ 
+pi = count * 4
 print("The resulting value is equal to: %0.10f" % pi)
